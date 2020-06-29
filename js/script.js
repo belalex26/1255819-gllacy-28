76f885e29aa93b2cf2686110788735ac31f6d1ec
belalex26 <61048853+belalex26@users.noreply.github.com>
2020-06-29 17:38:17 +0300

    слайдер, правки по валидатору

--- js/script.js
+++ js/script.js
@@ -52,3 +52,53 @@ if (evt.keyCode === 27) {
   }
 }
 });
+
+
+var dot1 = document.querySelector(".slide1");
+var dot2 = document.querySelector(".slide2");
+var dot3 = document.querySelector(".slide3");
+var slideText1 = document.querySelector(".slider__item1");
+var slideText2 = document.querySelector(".slider__item2");
+var slideText3 = document.querySelector(".slider__item3");
+var slide = document.querySelector(".site-wrapper");
+
+
+dot1.onclick = function () {
+  dot2.classList.remove('current');
+  dot3.classList.remove('current');
+  slideText2.classList.remove('slide-current');
+  slideText3.classList.remove('slide-current');
+  slide.classList.remove('site-wrapper-2');
+  slide.classList.remove('site-wrapper-3');
+
+  dot1.classList.add('current');
+  slideText1.classList.add('slide-current');
+  slide.classList.add('site-wrapper-1');
+};
+
+dot2.onclick = function () {
+  slide.classList.remove('site-wrapper-1');
+  slide.classList.remove('site-wrapper-3');
+  dot1.classList.remove('current');
+  dot3.classList.remove('current');
+  slideText1.classList.remove('slide-current');
+  slideText3.classList.remove('slide-current');
+
+  dot2.classList.add('current');
+  slideText2.classList.add('slide-current');
+  slide.classList.add('site-wrapper-2');
+};
+
+dot3.onclick = function () {
+  slide.classList.remove('site-wrapper-1');
+  slide.classList.remove('site-wrapper-2');
+  dot1.classList.remove('current');
+  dot2.classList.remove('current');
+  slideText1.classList.remove('slide-current');
+  slideText2.classList.remove('slide-current');
+
+  dot3.classList.add('current');
+  slideText3.classList.add('slide-current');
+  slide.classList.add('site-wrapper-3');
+
+};
